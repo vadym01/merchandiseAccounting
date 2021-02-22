21080ddbcf34a4c5733c5a01d0bdede3e22d9f7a
vadym <vasylaki95@gmail.com>
2021-02-22 12:47:15 +0200

    new incidentsController incidentsRepository incidents

---
+++ src/main/java/com/accounting/merchandiseAccounting/repository/repositoryImpl/IncidentsRepositoryImpl.java
@@ -0,0 +1,8 @@
+package com.accounting.merchandiseAccounting.repository.repositoryImpl;
+
+import com.accounting.merchandiseAccounting.repository.IncidentsRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IncidentsRepositoryImpl implements IncidentsRepository {
+}
