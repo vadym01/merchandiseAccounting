fe33f813a348dd585e9827c2c5bfc5717261b939
vadym <vasylaki95@gmail.com>
2021-02-22 10:45:27 +0200

    initial project structure

---
+++ src/main/java/com/accounting/merchandiseAccounting/MerchandiseAccountingApplication.java
@@ -0,0 +1,13 @@
+package com.accounting.merchandiseAccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchandiseAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchandiseAccountingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accounting/merchandiseAccounting/MerchandiseAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accounting.merchandiseAccounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchandiseAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
